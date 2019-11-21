4b885408d72bee1f747da24cf5b31513b6eff298
Olaf <o.olaf@hotmail.com>
2019-11-21 16:31:02 +0100

    Tooltip + animation

---
+++ Assets/PopUp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PopUp : MonoBehaviour
+{
+    private Animator controller;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        controller = GetComponent<Animator>();
+    }
+
+    public void PopUpToolTip(bool popup)
+    {
+        controller.SetBool("PopUp", popup);
+    }
+}
